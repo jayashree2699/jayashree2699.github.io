d4d4e39cdc206b04e1d67d18f9ca68fc44f254fc
Sujithra Rajan <63013044+Sujishark@users.noreply.github.com>
2021-07-29 19:37:57 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function login(){
+
+    let username='';
+    let password='';
+
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    fetch('http://localhost:3000/users').then(
+        response =>{
+            if(response.ok){
+                return response.json();
+            }}).then(userResponse=>{
+                let flag= false;
+                userDetails = userResponse;
+                userDetails.find(user =>{
+                    if(username == user.username && password == user.password ){
+                        flag =true;
+                    }
+                });
+                if(flag){
+                    window.open("./home.html");
+                }
+                else{
+                    document.getElementById("error").innerHTML=`<div class="w-150 alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Invalid Username or Password</strong> 
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>`;
+                }
+            });
+}
